e68da39d18855519a7a350f63e2aab3136def0ad
Mert Demir <mertdemir@Merts-MacBook-Air.local>
2025-08-21 14:16:50 +0300

    Prepare for Cloud Run deployment

--- models/notes_to_tags.py
+++ models/notes_to_tags.py
@@ -5,3 +5,5 @@ notes_to_tags = db.Table(
     db.Column("note_id", db.Integer, db.ForeignKey("notes.id"), primary_key=True),
     db.Column("tag_id", db.Integer, db.ForeignKey("tags.id"), primary_key=True)
 )
+
+
